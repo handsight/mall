7e95d03d0254ee7fd35fe92741dd335c739273d6
xieshijie <password>
2021-04-21 17:32:17 +0800

    打包插件3

--- mall-auth/src/main/java/com/mall/cloud/Oauth2Application.java
+++ mall-auth/src/main/java/com/mall/cloud/Oauth2Application.java
@@ -8,14 +8,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import tk.mybatis.spring.annotation.MapperScan;
 
 
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@MapperScan(basePackages = "com.mall.cloud.mapper")
 public class Oauth2Application {
     public static void main(String[] args) {
         SpringApplication.run(Oauth2Application.class, args);
